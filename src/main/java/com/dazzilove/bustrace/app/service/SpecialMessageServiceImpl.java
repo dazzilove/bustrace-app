79581287733be5771f830569923b914b88747290
10028467 <dazzilove@sk.com>
2020-03-16 01:37:45 +0900

    - 조회 방식 변경 및 sort 추가

--- src/main/java/com/dazzilove/bustrace/app/service/SpecialMessageServiceImpl.java
+++ src/main/java/com/dazzilove/bustrace/app/service/SpecialMessageServiceImpl.java
@@ -3,6 +3,12 @@ package com.dazzilove.bustrace.app.service;
 import com.dazzilove.bustrace.app.domain.SpecialMessage;
 import com.dazzilove.bustrace.app.repository.SpecialMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,14 +18,25 @@ import java.util.List;
 public class SpecialMessageServiceImpl implements SpecialMessageService {
 
     @Autowired
+    private MongoOperations mongoOperations;
+
+    @Autowired
     private SpecialMessageRepository specialMessageRepository;
 
     @Override
     public List<SpecialMessage> getSpecialMessageList(String routeId) {
-        List<SpecialMessage> specialMessageList = specialMessageRepository.findByRouteId(routeId);
-        if (specialMessageList == null) {
-            specialMessageList = new ArrayList<>();
-        }
-        return specialMessageList;
+
+        List<AggregationOperation> aggOperationlist = new ArrayList<AggregationOperation>();
+        if (routeId.length() > 0)
+            aggOperationlist.add(Aggregation.match(Criteria.where("routeId").is(routeId)));
+        aggOperationlist.add(Aggregation.sort(Sort.Direction.DESC, "createdAt"));
+
+        TypedAggregation<SpecialMessage> agg = Aggregation.newAggregation(SpecialMessage.class, aggOperationlist);
+
+        List<SpecialMessage> specialMessages = mongoOperations.aggregate(agg, SpecialMessage.class, SpecialMessage.class).getMappedResults();
+        if (specialMessages == null)
+            specialMessages = new ArrayList<>();
+
+        return specialMessages;
     }
 }
