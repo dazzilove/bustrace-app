5f1865f067fce41ae538f7440df9f607acffa291
dazzilove <dazzilove@gmail.com>
2020-04-12 21:24:14 +0900

    application.pid 추가

--- src/main/java/com/dazzilove/bustrace/app/AppApplication.java
+++ src/main/java/com/dazzilove/bustrace/app/AppApplication.java
@@ -2,12 +2,15 @@ package com.dazzilove.bustrace.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class AppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
+        SpringApplication app = new SpringApplication(AppApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
     }
 
 }
